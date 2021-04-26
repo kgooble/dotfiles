bac7158c211d26db70c049a2c9121a180772d1e8
Shahrin Khan <gooble.sk@gmail.com>
2021-04-26 13:28:52 -0400

    Add rbenv set up for Unity cocoapods installation

--- unity/setup.sh
+++ unity/setup.sh
@@ -5,6 +5,11 @@ echo "Installing JDK versions required for Unity..."
 brew install openjdk@8
 brew install openjdk@11
 
+echo "Installing Rbenv and Ruby versions required for iOS builds..."
+
+brew install rbenv
+rbenv install 2.6.0
+
 echo "Installing Unity..."
 
 brew install --cask unity-hub
